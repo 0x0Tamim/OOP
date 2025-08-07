4d6944b9eedeb9f29915f0730c1e7e367e828df2
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-08-08 03:45:27 +0600

    Practise-1 modified

--- Practise-1/src/Main.java
+++ Practise-1/src/Main.java
@@ -5,23 +5,46 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        Student[] students = new Student[5];
-        System.out.println(Arrays.toString(students));
+        //Student[] students = new Student[5];
+        //System.out.println(Arrays.toString(students));
 
-        Student tamim = new Student();
-        tamim.name = "Tamim Uz Zaman";
-        tamim.rollNo = 298;
+        Student tamim = new Student("Tamim",298,64);
+        Student abir = new Student();
+        Student random = new Student(tamim);
+        //tamim.name = "Tamim Uz Zaman";
+        //tamim.rollNo = 298;
 
         System.out.println(tamim);
         System.out.println(tamim.name);
         System.out.println(tamim.rollNo);
         System.out.println(tamim.marks);
 
+        System.out.println(abir.name);
+        System.out.println(abir.rollNo);
+        System.out.println(abir.marks);
+
+        System.out.println(random.name);
     }
 }
     class Student{
         String name;
         int rollNo;
-        float marks = 90;
+        float marks ;
+
+        Student(Student other){
+            this.name = other.name;
+            this.rollNo = other.rollNo;
+            this.marks = other.marks;
+        }
+        Student(){
+            this.name = "Abir";
+            this.rollNo = 86;
+            this.marks = 79;
+        }
+        Student(String name,int rollNo,float marks){
+          this.name = name;
+          this.rollNo = rollNo;
+          this.marks = marks;
+        }
 
     }
