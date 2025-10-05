da7ed10c913c0d4f5a30ee415d7a6866a6cb90dc
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-10-05 22:36:27 +0600

    More codes addede

---
+++ Practise- 3/src/polymorphism/Main.java
@@ -0,0 +1,15 @@
+package polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        Shapes square = new Square();
+        Circle circle = new Circle();
+
+        shape.area();
+        square.area();
+        circle.area();
+
+
+    }
+}
