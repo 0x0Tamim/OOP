477355bcd07651b0d4ab63e2237b35ff327a9fc0
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-10-06 07:36:07 +0600

    Learned about compile time and run time polymorphism

--- Practise- 3/src/polymorphism/Main.java
+++ Practise- 3/src/polymorphism/Main.java
@@ -16,3 +16,14 @@ public class Main {
 
     }
 }
+/*
+"Polymorphism means: One name, many forms"
+        In Java, it allows one method, object, or operator to behave differently based on the context.*/
+
+//        1️⃣ Compile-Time Polymorphism (Method Overloading)
+//
+//        ➡️ Same method name, different parameters (in the same class).
+//
+//        2️⃣ Runtime Polymorphism (Method Overriding)
+//
+//        ➡️ Same method name, same parameters – but defined in parent and overridden in child.
